ea86a5a78ba1f6d74f3d2f4fa6b0d7c3591a588a
BrandonHoeft <brandonmhoeft@gmail.com>
2019-02-03 21:14:14 -0600

    added smoothed trendline over time plots

--- rank_eda.py
+++ rank_eda.py
@@ -22,7 +22,7 @@ search = df.agg('std') == 0# schools with no variance rankings
 ranks_with_no_variance = df.agg('std').index[search].tolist()
 
 
-# Understand the center, variance of each School's ratings
+# Visualize the center, variance of each School's ratings #####################
 df2 = df.copy()
 df2[ranks_with_no_variance] = (df2[ranks_with_no_variance].apply(lambda x: np.random.normal(x, 0.25, df.shape[0])))
 
@@ -37,3 +37,31 @@ joypy.joyplot(df2[rank_by_lowest_median], overlap=3,
 
 plt.show()
 plt.savefig('Daily Program Rank Distribution.png')
+
+
+# Visualize the trend in daily ranking ########################################
+
+df = df.set_index('date')
+
+
+(df[rank_by_lowest_median[5:10]].rolling(7)
+.mean()
+.plot(color=['red', 'maroon', 'k', 'lightseagreen', 'b'],
+      title='Median Best Rank Programs #6 - #10 (seven-day moving avg)',
+      figsize=(8,6)))
+plt.savefig('Median best rank 6 to 10 trendline.png')
+
+
+(df[rank_by_lowest_median[:5]].rolling(7)
+.mean()
+.plot(color=['blueviolet', 'rebeccapurple','lavender', 'dodgerblue', 'orangered'],
+      title='Median Best Rank Programs #1 - #5 (seven-day moving avg)',
+      figsize=(8,6)))
+plt.savefig('Median best rank 1 to 5 trendline.png')
+
+
+# Raw data timeline, no smoothing...
+df[rank_by_lowest_median[5:10]].plot(color=['red', 'maroon', 'k', 'lightseagreen', 'b'],
+  title='Median Best Rank Programs #6 - #10 (seven-day moving avg)',
+  figsize=(8,6))
+
